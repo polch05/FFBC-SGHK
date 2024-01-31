3bcedb5d3895984d103c27485929e2e3afe970b4
polch05 <polch05@gmail.com>
2024-01-30 21:26:45 -0700

    Updated JavaScript Exercises to add CSS to console.log. Plus starting my new website HTML/CSS

--- JS Practice/exerciseGradeCalculator.js
+++ JS Practice/exerciseGradeCalculator.js
@@ -1,12 +1,13 @@
+
 let numericScore = "";
 let finalGrade = "";
 
 numericScore = prompt("Please, enter the students NUMERIC grade between 0-100: ");
-//console.log("The numeric grade that has been entered is: " + numericGrade);
+//console.log("The numeric grade that has been entered is: " + numericGrade)
 
 if (numericScore >= 90 && numericScore <= 100) {
     let finalGrade = "A";
-    console.log(`Score achieved equals ${numericScore}, which gives you a ` + finalGrade + " grade.");
+    console.log("Score achieved equals" + numericScore + ", which gives you an %cA Grade", `background:linear-gradient(#E66465, #9198E5); padding: 1rem; color:white; border-radius: .5em`);
 } else if (numericScore >= 80 && numericScore <= 89) {
     let finalGrade = "B";
     console.log(`Score achieved equals ${numericScore}, which gives you a ` + finalGrade + " grade.");
@@ -21,5 +22,5 @@ if (numericScore >= 90 && numericScore <= 100) {
     console.log(`Score achieved equals ${numericScore}, which gives you a ` + finalGrade + " grade.");
 }
 else {
-    console.log("No grade category is assigned BECAUSE the numeric grade entered is not recognized. Did you fail the exam :-0 ?")
+    console.log("%cNo Grade is assigned BECAUSE the numeric score that was entered is not recognized. Did you fail the exam :-0 ?", `background:linear-gradient(#E66465, #9198E5); padding: 1rem; color:white; border-radius: .5em`)
 }
